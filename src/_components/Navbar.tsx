33548727f5bfed60d1fcef0ca6700434345f6dc9
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-10 16:24:02 +0330

    [UPDATE] added next ui and components for navbar

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
-import ScrollButton from "./ScrollButton";
+import NavbarButtonList from "./NavbarButtonList";
+import DropDownMenu from "./DropDownMenu";
 
 const Navbar = () => {
     return (
         <div className="flex flex-wrap items-center justify-between p-16 font-poppins">
             <Link className="text-2xl" href={''}>Amir Zarei</Link>
-            <ScrollButton />
+            <div className="hidden lg:block">
+                <NavbarButtonList />
+            </div>
+            <div className="block lg:hidden">
+                <DropDownMenu />
+            </div>
         </div>
     );
 }
