b4e10fea9d740992a433df2cc88e8fec0278a78d
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-11 16:26:33 +0330

    [UPDATE] added styles and placements for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@nextui-org/react';
 import Image from 'next/image';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function HomePage() {
   return (
@@ -12,29 +11,42 @@ export default function HomePage() {
         height={500}
         alt="Picture of the author"
       /> */}
-        <div className='flex flex-col gap-y-1'>
+        <div className='flex flex-col gap-y-2'>
+          <div className='flex justify-center'>
+            <Image className='rounded-full mb-8'
+              src="/images/prof1.jpg"
+              width={261}
+              height={261}
+              alt="amir photo"
+            />
+          </div>
           <h1 className='text-darkGrey font-semibold text-base'>HELLO, I&apos;M</h1>
-          <h2 className='text-[3rem] font-extrabold'>Amir Zarei</h2>
-          <h3 className='text-darkGrey font-semibold text-3xl'>Frontend Developer</h3>
+          <h2 className='text-4xl font-extrabold'>Amir Zarei</h2>
+          <h3 className='text-darkGrey font-semibold text-base'>Frontend Developer</h3>
+          <div className='flex flex-wrap w-full justify-center gap-4'>
+            <Button className='font-semibold hover:bg-black hover:text-white' radius="full" variant="bordered">
+              Download CV
+            </Button>
+            <Button className='bg-black text-white font-semibold' radius="full">
+              Contact Info
+            </Button>
+          </div>
+          <div className='flex flex-wrap w-full justify-center gap-4'>
+            <Image
+              src="/images/linkedin.png"
+              width={32}
+              height={32}
+              alt="linkedin logo"
+            />
+            <Image
+              src="/images/github.svg"
+              width={32}
+              height={32}
+              alt="github logo"
+            />
+          </div>
         </div>
-
-        <div className='flex flex-wrap w-full justify-center gap-4'>
-          <Button radius="full" variant="bordered">
-            Download CV
-          </Button>
-          <Button radius="full">
-            Contact Info
-          </Button>
-        </div>
-
-        <div className='flex justify-center gap-4'>
-          <FaLinkedin />
-          <FaGithub />
-
-        </div>
-
       </div>
-
     </main>
   );
 }
