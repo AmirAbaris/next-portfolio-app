0eb4a8eb7fc6be5334b4641544712ce81595a016
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-11 08:19:41 +0330

    [UPDATE] added basic home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import { Button } from '@nextui-org/react';
+import Image from 'next/image';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 export default function HomePage() {
   return (
     <main className="container mx-auto">
-      <h1>content</h1>
+      <div className='flex flex-col w-full text-center'>
+        {/* <Image className='rounded-full'
+        src="/images/prof1.jpg"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      /> */}
+        <div className='flex flex-col gap-y-1'>
+          <h1 className='text-darkGrey font-semibold text-base'>HELLO, I&apos;M</h1>
+          <h2 className='text-[3rem] font-extrabold'>Amir Zarei</h2>
+          <h3 className='text-darkGrey font-semibold text-3xl'>Frontend Developer</h3>
+        </div>
+
+        <div className='flex flex-wrap w-full justify-center gap-4'>
+          <Button radius="full" variant="bordered">
+            Download CV
+          </Button>
+          <Button radius="full">
+            Contact Info
+          </Button>
+        </div>
+
+        <div className='flex justify-center gap-4'>
+          <FaLinkedin />
+          <FaGithub />
+
+        </div>
+
+      </div>
+
     </main>
   );
 }
