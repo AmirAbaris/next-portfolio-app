8c97b711d5f0a2d3e4cc216538b1ec87b9274bc7
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-12 14:30:09 +0330

    [REFACTOR] improved item placements

--- src/_components/AboutMe.tsx
+++ src/_components/AboutMe.tsx
@@ -3,20 +3,25 @@ import AboutMeBorderBox from './AboutMeBorderBox';
 
 const AboutMe = () => {
     return (
-        <div className='grid grid-rows-2 items-center gap-y-10 justify-center'>
-            <div className='w-[15rem] lg:w-[23rem] items-center justify-center'>
+        <div className='grid grid-cols-1 gap-y-10 items-center lg:grid-cols-2'>
+            <div className='w-[11rem] lg:w-[23rem] mx-auto'>
                 <Image
-                    className="rounded-3xl w-full h-auto aspect-square object-cover"
+                    className="rounded-3xl w-full h-auto object-cover aspect-square"
                     src="/images/prof2.jpg"
                     height={3024}
                     width={4032}
-                    sizes='100vw'
-                    alt="amirs secondary photo"
+                    alt="Amir's secondary photo"
                 />
             </div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 w-full justify-items-center'>
-                <AboutMeBorderBox iconPath='/images/medal.svg' title='Experience' subTitle='6 Months Frontend internship' />
+            <div className='grid grid-cols-1 gap-8 px-16 w-full sm:grid-cols-2 lg:px-0 lg:grid'>
+                <AboutMeBorderBox iconPath='/images/medal.svg' title='Experience' subTitle='6 Months Frontend Internship' />
                 <AboutMeBorderBox iconPath='/images/person.svg' title='Education' subTitle='Computer Science Student' />
+                <p className='col-span-full'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis reprehenderit et laborum, rem,
+                    dolore eum quod voluptate exercitationem nobis, nihil esse debitis maxime facere minus sint delectus velit
+                    in eos quo officiis explicabo deleniti dignissimos. Eligendi illum libero dolorum cum laboriosam corrupti quidem,
+                    reiciendis ea magnam? Nulla, impedit fuga!
+                </p>
             </div>
         </div>
     );
--- src/_components/SectionHeader.tsx
+++ src/_components/SectionHeader.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-
 interface SectionHeaderProps {
     title: string;
     subTitle: string;
 }
 const SectionHeader = ({ title, subTitle }: SectionHeaderProps) => {
     return (
-        <div className='flex flex-col gap-y-2 text-center'>
+        <div className='flex flex-col text-center'>
             <h1 className='text-base text-darkGrey'>{subTitle}</h1>
-            <h2 className='text-5xl text-black font-extrabold'>{title}</h2>
+            <h2 className='text-[2rem] text-black font-extrabold md:text-5xl'>{title}</h2>
         </div>
     );
 }
