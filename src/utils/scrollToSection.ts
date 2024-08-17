1d2f42826e8893c1f10a148e5734784927a213f2
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-17 16:11:10 +0330

    [REFACTOR] changed the scroll y setting var in utils

--- src/utils/scrollToSection.ts
+++ src/utils/scrollToSection.ts
@@ -5,7 +5,7 @@ import { type TitleType } from "~/types/navigation";
 const scrollToSection = (key: TitleType) => {
     const section = document.getElementById(key);
     if (section) {
-        const y = section.getBoundingClientRect().top + window.scrollY - 128;
+        const y = section.getBoundingClientRect().top + window.scrollY - 64;
         window.scrollTo({ top: y, behavior: 'smooth' });
     }
 };
