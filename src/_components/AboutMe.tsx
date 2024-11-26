11317325f02da4859087e0808acd12b00993b87e
Amir Abaris <thisisamirabaris@gmail.com>
2024-11-26 12:18:09 +0330

    updated about me status

--- src/_components/AboutMe.tsx
+++ src/_components/AboutMe.tsx
@@ -1,21 +1,40 @@
-import AboutMeBorderBox from './AboutMeBorderBox';
-import ImageWithLoader from './ImageWithLoader';
+import AboutMeBorderBox from "./AboutMeBorderBox";
+import ImageWithLoader from "./ImageWithLoader";
 
 const AboutMe = () => {
-    return (
-        <div className='flex flex-col gap-8 items-center lg:flex-row'>
-            <div className='mx-auto w-4/5 md:w-2/3 lg:w-1/2'>
-                <ImageWithLoader src="/images/prof2.jpg" alt="Amir's secondary photo" borderRadius='rounded-3xl' className='aspect-square' />
-            </div>
-            <div className='grid grid-cols-1 gap-8 w-full sm:grid-cols-2 lg:px-0'>
-                <AboutMeBorderBox iconPath='/images/medal.svg' title='Experience' subTitle='Full time frontend dev at Nilasoft' />
-                <AboutMeBorderBox iconPath='/images/person.svg' title='Education' subTitle='Computer Science Student' />
-                <p className='col-span-full'>
-                    I kicked off my journey with a 6-month internship, diving deep into Angular magical world. After mastering components, services, and all those *ngIf/*ngFor goodies, Ive continued to grow my expertise in Angulars ecosystem. Now, Im on a mission to craft snappy, seamless user experiences and bring powerful enterprise applications to life using Angular robust framework.
-                </p>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col items-center gap-8 lg:flex-row">
+      <div className="mx-auto w-4/5 md:w-2/3 lg:w-1/2">
+        <ImageWithLoader
+          src="/images/prof2.jpg"
+          alt="Amir's secondary photo"
+          borderRadius="rounded-3xl"
+          className="aspect-square"
+        />
+      </div>
+      <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:px-0">
+        <AboutMeBorderBox
+          iconPath="/images/medal.svg"
+          title="Experience"
+          subTitle="frontend dev at Nilasoft for 3 months + 6 months internship"
+        />
+        <AboutMeBorderBox
+          iconPath="/images/person.svg"
+          title="Education"
+          subTitle="Computer Science Student"
+        />
+        <p className="col-span-full">
+          I kicked off my journey with a 6-month internship, diving deep into
+          Angular&apos;s world. After mastering components, services, and core
+          concepts, I decided to explore new horizons and transitioned to
+          Next.js. Now, I&apos;m on a mission to craft snappy, seamless user
+          experiences and bring powerful applications to life using React and
+          Next.js&apos;s robust ecosystem, leveraging features like server-side
+          rendering and static site generation.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default AboutMe;
