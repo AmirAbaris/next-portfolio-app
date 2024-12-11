bbb22dbdc6b69b44c31614ca51bb7457e73ad2a0
Amir Abaris <thisisamirabaris@gmail.com>
2024-12-12 00:11:03 +0330

    fixed mobile view navbar menu icon state change

--- src/_components/DropDownMenu.tsx
+++ src/_components/DropDownMenu.tsx
@@ -1,46 +1,62 @@
-'use client';
+"use client";
 
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from '@nextui-org/dropdown';
-import React from 'react';
-import { CiMenuBurger } from 'react-icons/ci';
-import { IoMdClose } from 'react-icons/io';
-import { type TitleType } from '~/types/navigation';
-import scrollToSection from '~/utils/scrollToSection';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/dropdown";
+import React, { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+import { type TitleType } from "~/types/navigation";
+import scrollToSection from "~/utils/scrollToSection";
 
 const DropDownMenu = () => {
-    const [open, setOpen] = React.useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
 
-    const handleButtonClick = (key: TitleType) => {
-        scrollToSection(key);
-    };
+  const handleButtonClick = (key: TitleType) => {
+    scrollToSection(key);
+  };
 
-    return (
-        <div className='cursor-pointer'>
-            <Dropdown>
-                <DropdownTrigger>
-                    <div onClick={() => setOpen(!open)}>
-                        {open
-                            ? <IoMdClose />
-                            : <CiMenuBurger />}
-                    </div>
-                </DropdownTrigger>
-                <DropdownMenu>
-                    <DropdownItem key="about" onClick={() => handleButtonClick('about')}>
-                        About
-                    </DropdownItem>
-                    <DropdownItem key="experience" onClick={() => handleButtonClick('experience')}>
-                        Experience
-                    </DropdownItem>
-                    <DropdownItem key="project" onClick={() => handleButtonClick('project')}>
-                        Project
-                    </DropdownItem>
-                    <DropdownItem key="contact" onClick={() => handleButtonClick('contact')}>
-                        Contact
-                    </DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
-        </div>
-    );
+  function handleMenuButton(value: boolean) {
+    setOpen(!value);
+  }
+
+  return (
+    <div className="cursor-pointer">
+      <Dropdown>
+        <DropdownTrigger>
+          <div onClick={() => handleMenuButton(open)}>
+            {open ? <IoMdClose /> : <CiMenuBurger />}
+          </div>
+        </DropdownTrigger>
+        <DropdownMenu>
+          <DropdownItem key="about" onClick={() => handleButtonClick("about")}>
+            About
+          </DropdownItem>
+          <DropdownItem
+            key="experience"
+            onClick={() => handleButtonClick("experience")}
+          >
+            Experience
+          </DropdownItem>
+          <DropdownItem
+            key="project"
+            onClick={() => handleButtonClick("project")}
+          >
+            Project
+          </DropdownItem>
+          <DropdownItem
+            key="contact"
+            onClick={() => handleButtonClick("contact")}
+          >
+            Contact
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
 };
 
 export default DropDownMenu;
