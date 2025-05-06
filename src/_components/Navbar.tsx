3809f9152c58a766905e9c7010a55d87ca1bce74
Amir Abaris <thisisamirabaris@gmail.com>
2025-05-06 18:16:36 +0330

    refactor DropDownMenu and Navbar components for improved readability and dynamic loading

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -1,25 +1,29 @@
-import NavbarButtonList from "./NavbarButtonList";
-import DropDownMenu from "./DropDownMenu";
-import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/navbar";
+import NavbarButtonList from './NavbarButtonList';
 
-const  NavbarItems = () => {
-    return (
-        <Navbar className="mb-12 lg:mb-56">
-            <NavbarBrand>
-                <p className="font-bold text-2xl">Amir Zarei</p>
-            </NavbarBrand>
-            <div className="hidden lg:flex">
-                <NavbarContent justify="center">
-                    <NavbarButtonList />
-                </NavbarContent>
-            </div>
-            <div className="flex lg:hidden">
-                <NavbarContent>
-                    <DropDownMenu />
-                </NavbarContent>
-            </div>
-        </Navbar>
-    );
-}
+import {Navbar, NavbarBrand, NavbarContent} from '@nextui-org/navbar';
+import dynamic from 'next/dynamic';
+const DropDownMenu = dynamic(() => import('./DropDownMenu'), {
+  loading: () => null,
+});
+
+const NavbarItems = () => {
+  return (
+    <Navbar className='mb-12 lg:mb-56'>
+      <NavbarBrand>
+        <p className='font-bold text-2xl'>Amir Zarei</p>
+      </NavbarBrand>
+      <div className='hidden lg:flex'>
+        <NavbarContent justify='center'>
+          <NavbarButtonList />
+        </NavbarContent>
+      </div>
+      <div className='flex lg:hidden'>
+        <NavbarContent>
+          <DropDownMenu />
+        </NavbarContent>
+      </div>
+    </Navbar>
+  );
+};
 
 export default NavbarItems;
